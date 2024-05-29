d64ff12823fa70d5acd6c6102e0f6c47fca45b57
Gianpaolo Barone <gpbarone87@gmail.com>
2024-05-29 15:50:49 +0200

    test

--- templates/front-page.php
+++ templates/front-page.php
@@ -52,7 +52,7 @@
     foreach ($flavors as $i => $post) : setup_postdata($post); 
     
       $link = get_field('sf_pdp_button', $post->ID);
-      SF_test($link);
+      var_dump($link);
     // $link_target = $link['target'] ? $link['target'] : '_self';
       $column_direction = ($i % 2 === 0) ? ' flex-md-row' : ' flex-md-row-reverse'; ?>
 
